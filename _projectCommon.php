4f9bf738ec0cd64b38da3f9b285122d05ba2e577
spearce <spearce>
2009-10-02 15:17:56 +0000

    Point to JGit/EGit repositories on egit.eclipse.org, and to Contributor Guide Add screenshot of plugin.

--- _projectCommon.php
+++ _projectCommon.php
@@ -9,13 +9,21 @@
 	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
+  $Nav->addNavSeparator("JGit",   "/egit/");
+  $Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Source", "http://egit.eclipse.org/egit/parallelip-jgit.git", "_self", 1);
+  $Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Contributor Guide", "http://wiki.eclipse.org/EGit/Contributor_Guide", "_self", 1);
+
 	$Nav->addNavSeparator("EGit", 	"/egit/");
-	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;About This Project", "/projects/project_summary.php?projectid=technology.egit", "_self", 1);
-	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Project Proposal", "/proposals/egit/", "_self", 1);
+  $Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Source", "http://egit.eclipse.org/egit/parallelip-egit.git", "_self", 1);
+  $Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Contributor Guide", "http://wiki.eclipse.org/EGit/Contributor_Guide", "_self", 1);
 
 	$Nav->addNavSeparator("Users", "/egit/", "_self", 1);
 	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Downloads", "downloads.php", "_self", 1);
 	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Installation", "install.php", "_self", 1);
 	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;FAQ (wiki)", "http://wiki.eclipse.org/index.php/EGit/FAQ", "_self", 1);
+
+  $Nav->addNavSeparator("About",   "/egit/");
+  $Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;About This Project", "/projects/project_summary.php?projectid=technology.egit", "_self", 1);
+  $Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Project Proposal", "/proposals/egit/", "_self", 1);
 
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	# Begin: page-specific settings.  Change these. 
 	$pageTitle 		= "EGit";
-	$pageKeywords	= "Git, Team Provider, VCS, DVCS";
+	$pageKeywords	= "Git, Team Provider, VCS, DVCS, JGit, EGit";
 	$pageAuthor		= "Shawn Pearce";
 	
 	# Add page-specific Nav bars here
@@ -19,14 +19,27 @@
 
 <div id="maincontent">
 	<div id="midcolumn">
-		<h1>$pageTitle</h1>
+		<h1>$pageTitle</h1>		
 		<p>
-		EGit is an Eclipse Team provider for <a href="http://git-scm.com/">Git</a>.
-		Git is a distributed SCM, which means every developer has a full copy of all
-		history of every revision of the code, making queries against the history
-		very fast and versatile. The EGit project is implementing Eclipse tooling
-		for the JGit Java implementation of Git.
+		The EGit project maintains two components:
 		</p>
+
+		<p>
+		<b>JGit</b>, a <a href="http://www.eclipse.org/org/documents/edl-v10.php">EDL/BSD</a>
+		licensed, lightweight, pure Java library implementing the <a href="http://git-scm.com/">Git</a>
+		version control file access routines, network protocols, and core version
+		control algorithms.
+		</p>
+
+		<p>
+		<b>EGit</b>, an <a href="http://www.eclipse.org/org/documents/epl-v10.php">EPL</a>
+		licensed Eclipse Team provider built on top of JGit.
+		</p>
+
+    <div>
+      <h6>EGit Eclipse Plugin Screenshot</h6>
+      <a href="images/fm0.3_800x500.png"><img src="images/fm0.3_800x500.png" width="400" height="250" border="0" /></a>
+    </div>
 	</div>
 
 	<div id="rightcolumn">
