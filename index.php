061d25a70a4a4e6af677df3e9928b0e238a8d46c
caniszczyk <caniszczyk>
2010-02-02 19:41:08 +0000

    CSS is painful

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	$pageTitle 		= "EGit";
 
-	$html  = '<div id="midcolumn">';
-	$html .= file_get_contents('_index.html');
-	$html .= "</div>";
+	# Paste your HTML content between the EOHTML markers!
+	$html = file_get_contents('_index.html');
 	
 	# Generate the web page
 	$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
