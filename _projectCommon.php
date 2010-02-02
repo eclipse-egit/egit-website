85290a8e1e1b9ca2a9d78746781093bb1cae60e8
caniszczyk <caniszczyk>
2010-02-02 18:21:25 +0000

    updating project common

--- _projectCommon.php
+++ _projectCommon.php
@@ -28,8 +28,8 @@
 	$Menu->addMenuItem("Support", "/egit/support", "_self");
 	$Menu->addMenuItem("Developers", "/egit/developers", "_self");
 	
-	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style.css"/>' . "\n\t");
-	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style2.css"/>' . "\n\t");
+	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/egit/style.css"/>' . "\n\t");
+	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/egit/style2.css"/>' . "\n\t");
 	
 	$App->Promotion = TRUE;
 ?>
