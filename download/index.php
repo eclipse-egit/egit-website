b09508e16d716231964c80b8e30db53236bdec94
Matthias Sohn <matthias.sohn@sap.com>
2013-09-17 13:23:47 +0200

    Update download page for EGit 3.0.3
    Change-Id: Ie3c54aa9d69d7f3c8ba8dcdf35f2a283eaae50be
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- download/index.php
+++ download/index.php
@@ -1,6 +1,6 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	$pageTitle 		= "EGit - Download";
-	$version      = "3.0.1.201307141637-r";
+	$version      = "3.0.3.201309161630-r";
 
 	$html  = '<div id="midcolumn">';
 	$html .= file_get_contents('_index.html');
