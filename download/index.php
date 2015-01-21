036ee821a7658dbd8b7061a133a6a395d69ca979
Matthias Sohn <matthias.sohn@sap.com>
2015-01-22 00:44:35 +0100

    Update download page for 3.6.2.201501210735-r
    Change-Id: Id4420766117c7faa453f38eaeaba8f0876d7f3f9
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- download/index.php
+++ download/index.php
@@ -1,6 +1,6 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	$pageTitle 		= "EGit - Download";
-	$version      = "3.6.1.201501031845-r";
+	$version      = "3.6.2.201501210735-r";
 
 	$html  = '<div id="midcolumn">';
 	$html .= file_get_contents('_index.html');
