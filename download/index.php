ba9534258571fe7aefbab05c95856ab4ec55f0ad
Matthias Sohn <matthias.sohn@sap.com>
2018-09-17 22:53:08 +0200

    Update download page for 5.1.1.201809181055-r
    Change-Id: I9fabd0a049ed13c9eaaca089baaf64fba42bf7f6
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- download/index.php
+++ download/index.php
@@ -19,7 +19,7 @@ $App = new App();
 include ($App->getProjectCommon());
 $Theme->setPageTitle("EGit - Download");
 
-$version = "5.0.3.201809091024-r";
+$version = "5.1.1.201809181055-r";
 $html = file_get_contents('_index.html');
 $html = preg_replace('/@VERSION@/', $version, $html);
 $Theme->setHtml($html);
