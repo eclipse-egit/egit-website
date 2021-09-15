5d686b3245908bba0844ed773225cee27a570357
Matthias Sohn <matthias.sohn@sap.com>
2021-09-15 16:42:36 +0200

    Update for 5.13.0.202109080827-r
    Change-Id: I9a4b8173052bb30e3fe8e9f44406fed13e76f410

--- download/index.php
+++ download/index.php
@@ -19,7 +19,7 @@ $App = new App();
 include ($App->getProjectCommon());
 $Theme->setPageTitle("EGit - Download");
 
-$version = "5.12.0.202106070339-r";
+$version = "5.13.0.202109080827-r";
 $html = file_get_contents('_index.html');
 $html = preg_replace('/@VERSION@/', $version, $html);
 $Theme->setHtml($html);
