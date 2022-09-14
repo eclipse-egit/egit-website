37cc5566ac3296d9d43058c4e5f6e912e16bf153
Matthias Sohn <matthias.sohn@sap.com>
2022-09-14 13:42:31 +0200

    Update for 6.3.0.202209071007-r
    Change-Id: I2415922b6027a23f741ce1f540eb3c911c63912e

--- download/index.php
+++ download/index.php
@@ -19,7 +19,7 @@ $App = new App();
 include ($App->getProjectCommon());
 $Theme->setPageTitle("EGit - Download");
 
-$version = "6.2.0.202206071550-r";
+$version = "6.3.0.202209071007-r";
 $html = file_get_contents('_index.html');
 $html = preg_replace('/@VERSION@/', $version, $html);
 $Theme->setHtml($html);
