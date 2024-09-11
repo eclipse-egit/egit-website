c21ec290f9872906fa66f50fd46f46bfad1ba122
Matthias Sohn <matthias.sohn@sap.com>
2024-09-11 15:43:33 +0200

    Update for release 7.0.0.202409031743-r
    Change-Id: I4afed2c654f69e8d16c6cb315e30c1508fe42157

--- download/index.php
+++ download/index.php
@@ -19,7 +19,7 @@ $App = new App();
 include ($App->getProjectCommon());
 $Theme->setPageTitle("EGit - Download");
 
-$version = "6.8.0.202311291450-r";
+$version = "7.0.0.202409031743-r";
 $html = file_get_contents('_index.html');
 $html = preg_replace('/@VERSION@/', $version, $html);
 $Theme->setHtml($html);
